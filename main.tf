5a0e998522acf8dcfd6f54107c4c6e792646004e
demanou alain <demanou_nguena@hotmail.com>
2026-09-15 18:00:03 -0400

    Change IAM trust policy v8

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_internet_gateway" "customIGW" {
   }
 }
 
-#Deploy a Route Table v2
+#Deploy a Route Table v3
 resource "aws_route_table" "publicRT" {
   vpc_id = aws_vpc.customVPC.id #Attached RT to VPC
 
